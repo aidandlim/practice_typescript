db9da38d468f1b3ebee663a5c768f245edfc78b6
aidandlim <aidandlim@gmail.com>
2020-01-21 17:47:16 -0800

    update modules

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
 var person = {
     name: 'Aidan',
-    age: 28
+    age: 28,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
-console.log(person.nickname);
+// let favoriteActivities: string[];
+// favoriteActivities = ['Sports'];
+person.role.push('admin');
+// person.role[1] = 10;
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hoddy = _a[_i];
+    console.log(hoddy.toUpperCase());
+    // console.log(hoddy.map());
+}
